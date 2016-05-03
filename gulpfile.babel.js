54915739b4a66132275dc520d19e1af788516733
phweb-note <yvv@ugraweb.ru>
2016-05-04 01:04:12 +0500

    обновил файл сборки

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,9 +4,11 @@ import gulp from 'gulp';
 import pug from 'gulp-pug';
 import notify from 'gulp-notify';
 import plumber from 'gulp-plumber';
-import less from 'gulp-less';
-import postCSS from 'gulp-postcss';
 import imageMin from 'gulp-imagemin';
+
+import less from 'gulp-less';
+import LessAutoPrefix from 'less-plugin-autoprefix';
+import LessPluginCleanCSS from 'less-plugin-clean-css';
 
 import del from 'del';
 import browserSync from 'browser-sync';
@@ -16,11 +18,12 @@ browserSync.create();
 const plumberOptions = {
   errorHandler: notify.onError()
 };
+const develop = true;
 
 export const clean = () => del('./build');
 
 export function images() {
-  return gulp.src('./src/images/**/*')
+  return gulp.src('./src/images/**/*', {since: gulp.lastRun('images')})
     .pipe(imageMin({
       svgoPlugins: [{
         convertPathData: false
@@ -30,33 +33,42 @@ export function images() {
 }
 
 export function styles() {
-  let browsers = [
-    '> 1%',
-    'last 2 versions',
-    'Firefox ESR',
-    'Opera 12.1'
-  ];
-  return gulp.src('./src/styles/*.less')
+  let autoPrefix = new LessAutoPrefix({
+    browsers: [
+      '> 1%',
+      'last 2 versions',
+      'Firefox ESR',
+      'Opera 12.1'
+    ]
+  });
+  let plugins = [autoPrefix];
+
+  if (!develop) {
+    let cleanCSS = new LessPluginCleanCSS({ advanced: true });
+    plugins.push(cleanCSS);
+  }
+
+  return gulp.src('./src/styles/*.less', {since: gulp.lastRun('styles')})
     .pipe(plumber(plumberOptions))
     .pipe(less({
-      paths: ['node_modules']
+      paths: ['node_modules'],
+      plugins: plugins
     }))
-    .pipe(postCSS([
-      require('autoprefixer')({
-        browsers: browsers
-      })
-    ]))
     .pipe(gulp.dest('./build/css'));
 }
 
 export function views() {
-  return gulp.src([
+  let path = [
     './src/views/*.pug',
     '!./src/views/layout.pug'
-  ])
+  ];
+  return gulp.src(path, {since: gulp.lastRun('views')})
     .pipe(plumber(plumberOptions))
     .pipe(pug({
-      pretty: true
+      pretty: true,
+      data: {
+        dev: develop
+      }
     }))
     .pipe(gulp.dest('./build'));
 }
